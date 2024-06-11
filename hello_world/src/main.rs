d89e32b7f3e438c4b65d706c07dc59d7e7a61160
Yono1112 <yuma.ohno.sss@gmail.com>
2024-06-11 20:52:06 +0900

    modify hello_world code

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    print_str("Satoshi Nakamoto");
+
+    let str = String::from("HogeHoge");
+    print_str(str.as_str());
+}
+
+fn print_str(str: &str) {
+    println!("Hello, world {}!", str);
 }
