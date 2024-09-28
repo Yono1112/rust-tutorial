56d7c9bacd325b36e5658b053dc4ebcc4e5f0075
Yono1112 <yuma.ohno.sss@gmail.com>
2024-09-28 11:48:13 +0900

    webserv: tcpでクライアントからの接続を受け取る

--- webserv/src/main.rs
+++ webserv/src/main.rs
@@ -1,3 +1,26 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
