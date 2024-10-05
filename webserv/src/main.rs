e2fb39c2295ff761c1c12522b41cbdc15fa55881
Yono1112 <yuma.ohno.sss@gmail.com>
2024-10-06 01:29:22 +0900

    webserv: htmlをresponseとしてクライアントに返すように変更

--- webserv/src/main.rs
+++ webserv/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::{Read, Write};
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -16,10 +17,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("index.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    println!("Response: {}", response);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
