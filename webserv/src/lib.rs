d3e79ff556d4ab41f8c32a8419d725ef8feb5094
Yono1112 <yuma.ohno@purpom-media-lab.com>
2024-10-10 22:02:19 +0900

    webserv: マルチスレッドでのwebservが動いた

--- webserv/src/lib.rs
+++ webserv/src/lib.rs
@@ -1,7 +1,13 @@
+use std::sync::Mutex;
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+
+type Job = Box<dyn FnOnce() + Send + 'static>; // Job型はFnOnceトレイト(FnOnceトレイトは引数を取らず、戻り値を返さないクロージャを表す)を実装するクロージャを表す
 
 pub struct ThreadPool{
   workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -10,19 +16,24 @@ impl ThreadPool {
 
     let mut workers = Vec::with_capacity(size); // size分の要素を持つVecを作成
 
+    let (sender, receiver) = mpsc::channel(); // チャンネルを作成
+    let receiver = Arc::new(Mutex::new(receiver));
+
     for id in 0..size {
       // thread::spawn(|| {})で新しいスレッドを生成し、そのスレッドが実行するクロージャを引数に取る
       // このクロージャは何もしない
-      workers.push(Worker::new(id));
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
-    ThreadPool { workers }
+    ThreadPool { workers, sender }
   }
 
   pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static
   {
-    f();
+    let job: Job = Box::new(f);
+
+    self.sender.send(job).unwrap();
   }
 }
 
@@ -32,8 +43,14 @@ struct Worker {
 }
 
 impl Worker {
-  fn new(id: usize) -> Worker {
-    let thread = thread::spawn(|| {});
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let thread = thread::spawn(move || {
+      loop {
+        let job = receiver.lock().unwrap().recv().unwrap();
+        println!("Worker {} got a job; executing.", id);
+        job();
+      }
+    });
     Worker { id, thread }
   }
 }
